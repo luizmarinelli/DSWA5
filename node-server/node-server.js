7454b117c848197ea492c727c76c635d32a7f18b
Dev ifsp ptb <dev.ifsp.ptb@gmail.com>
2020-02-17 19:55:14 -0300

    Estrutura de pastas

---
+++ node-server/node-server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(
+    function(req, res){
+        res.writeHead(200, {
+            'Content-Type':'text/plain'
+        });
+        res.end('Criado!');
+    }
+).listen(3000, '127.0.0.1')
